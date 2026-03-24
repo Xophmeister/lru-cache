1a0c8e5614764b3c547b5ab55caaec686b4e50d1
Christopher Harrison <christopher.harrison@tweag.io>
2026-03-24 20:37:32 +0000

    Implemented Copilot's suggestions

--- tests/run.scm
+++ tests/run.scm
@@ -28,50 +28,74 @@
     (lru-cache-set! cache 'bar 456)
     (lru-cache-set! cache 'quux 789)
 
-    (test "simple ordering" '(quux bar foo) (lru-cache-keys cache))
+    (test "keys are ordered most- to least-recently inserted"
+          '(quux bar foo) (lru-cache-keys cache))
 
-    (test "fetch last" 123 (lru-cache-ref cache 'foo))
-    (test "order after fetch" '(foo quux bar) (lru-cache-keys cache))
+    (test "ref returns the value for an existing key"
+          123 (lru-cache-ref cache 'foo))
 
-    (test "fetch middle" 789 (lru-cache-ref cache 'quux))
-    (test "order after fetch" '(quux foo bar) (lru-cache-keys cache))
+    (test "ref promotes the least-recent key to most-recent"
+          '(foo quux bar) (lru-cache-keys cache))
 
-    (test "fetch first" 789 (lru-cache-ref cache 'quux))
-    (test "order after fetch" '(quux foo bar) (lru-cache-keys cache))
+    (test "ref on a middle key returns its value"
+          789 (lru-cache-ref cache 'quux))
+
+    (test "ref promotes a middle key to most-recent"
+          '(quux foo bar) (lru-cache-keys cache))
+
+    (test "ref on the most-recent key returns its value"
+          789 (lru-cache-ref cache 'quux))
+
+    (test "ref on the most-recent key does not change ordering"
+          '(quux foo bar) (lru-cache-keys cache))
 
     (lru-cache-set! cache 'bar 999)
-    (test "order after set" '(bar quux foo) (lru-cache-keys cache)))
+    (test "set! on an existing key promotes it to most-recent"
+          '(bar quux foo) (lru-cache-keys cache)))
 
   (test-group "eviction"
     (define cache (make-lru-cache 2))
 
     (lru-cache-set! cache 'foo 123)
     (lru-cache-set! cache 'bar 456)
-    (test "at capacity" (lru-cache-capacity cache) (lru-cache-size cache))
+    (test "size equals capacity when full"
+          (lru-cache-capacity cache) (lru-cache-size cache))
 
     (lru-cache-set! cache 'quux 789)
-    (test "still at capacity" (lru-cache-capacity cache) (lru-cache-size cache))
-    (test "last key evicted" '(quux bar) (lru-cache-keys cache)))
+    (test "size does not exceed capacity after another insert"
+          (lru-cache-capacity cache) (lru-cache-size cache))
+
+    (test "least-recently used key is evicted when capacity is exceeded"
+          '(quux bar) (lru-cache-keys cache)))
 
   (test-group "errors"
     (define cache (make-lru-cache))
 
-    (test-error "fetch non-existent key" (lru-cache-ref cache 'foo))
-    (test-error "delete non-existent key" (lru-cache-delete! cache 'foo)))
+    (test-error "ref raises an error for a non-existent key"
+               (lru-cache-ref cache 'foo))
+
+    (test-error "delete! raises an error for a non-existent key"
+               (lru-cache-delete! cache 'foo)))
 
   (test-group "empty cache"
     (define cache (make-lru-cache))
 
-    (test "size" 0 (lru-cache-size cache))
-    (test "no such key" #f (lru-cache-has-key? cache 'foo))
-    (test "keyless" '() (lru-cache-keys cache)))
+    (test "size is zero for a fresh cache"
+          0 (lru-cache-size cache))
+
+    (test "has-key? returns #f for any key in an empty cache"
+          #f (lru-cache-has-key? cache 'foo))
+
+    (test "keys returns an empty list for an empty cache"
+          '() (lru-cache-keys cache)))
 
   (test-group "edge case: capacity 1"
     (define cache (make-lru-cache 1))
 
     (lru-cache-set! cache 'foo 123)
     (lru-cache-set! cache 'bar 456)
-    (test "last key survives" '(bar) (lru-cache-keys cache)))
+    (test "only the most-recently inserted key survives with capacity 1"
+          '(bar) (lru-cache-keys cache)))
 
   (test-group "thunk caching"
     (define cache (make-lru-cache))
@@ -82,25 +106,32 @@
       123)
 
     (lru-cache-ref cache 'foo thunk)
-    (test "thunk discharged" 1 counter)
+    (test "ref with thunk evaluates the thunk on a cache miss"
+          1 counter)
 
     (define fetch (lru-cache-ref cache 'foo))
-    (test "thunk cached" 1 counter)
-    (test "fetch" 123 fetch))
+    (test "ref with thunk does not re-evaluate on a cache hit"
+          1 counter)
+
+    (test "ref returns the cached value produced by the thunk"
+          123 fetch))
 
   (test-group "memoise"
     (define counter 0)
     (define (fib n)
       (set! counter (+ counter 1))
       (cond
-        ((eq? n 0) 1)
-        ((eq? n 1) 1)
+        ((= n 0) 1)
+        ((= n 1) 1)
         (else (+ (fib (- n 1)) (fib (- n 2))))))
 
     (set! fib (memoise/lru fib))
 
-    (test "compute" '(1 1 2 3 5) (map fib (iota 5)))
-    (test "using cached results" 5 counter))
+    (test "memoised function returns correct results"
+          '(1 1 2 3 5) (map fib (iota 5)))
+
+    (test "memoised function reuses cached results instead of recomputing"
+          5 counter))
 
   (test-group "delete and clear"
     (define cache (make-lru-cache))
@@ -109,9 +140,11 @@
     (lru-cache-set! cache 'bar 456)
 
     (lru-cache-delete! cache 'foo)
-    (test "key deleted" '(bar) (lru-cache-keys cache))
+    (test "delete! removes the specified key from the cache"
+          '(bar) (lru-cache-keys cache))
 
     (lru-cache-clear! cache)
-    (test "cache cleared" 0 (lru-cache-size cache))))
+    (test "clear! removes all entries from the cache"
+          0 (lru-cache-size cache))))
 
 (test-exit)
